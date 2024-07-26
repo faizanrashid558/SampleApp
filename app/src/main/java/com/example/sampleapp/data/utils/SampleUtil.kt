48109e2c16335f29d91300cc53f34d61df703cc8
faizan rashid <faizanrashid558@gmail.com>
2024-07-26 10:20:02 +0500

    3rd commit

--- app/src/main/java/com/example/sampleapp/data/utils/SampleUtil.kt
+++ app/src/main/java/com/example/sampleapp/data/utils/SampleUtil.kt
@@ -5,6 +5,8 @@ import javax.inject.Inject
 class SampleUtil @Inject constructor(private val logger: Logger){
     init {
         logMsg()
+        logMsg2()
+        logMsg3()
     }
 
     private fun logMsg() {
@@ -12,6 +14,10 @@ class SampleUtil @Inject constructor(private val logger: Logger){
     }
 
     private fun logMsg2() {
+        logger.logd("Sample Util Test message")
+    }
+
+    private fun logMsg3() {
         logger.logd("Sample Util Test message")
     }
 }
